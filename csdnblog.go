34c4ec8f70d8f6f0275917fe09d6c3e477a37479
Tong Sun <suntong@cpan.org>
2016-10-21 14:58:21 -0400

    - [*] csdnblog works for test run

--- csdnblog.go
+++ csdnblog.go
@@ -11,6 +11,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sort"
 )
 
 ////////////////////////////////////////////////////////////////////////////
@@ -24,11 +25,20 @@ type Blog struct {
 	Blog  []string
 }
 
+// ByDate implements sort.Interface for []Blog based on the Date field.
+type ByDate []Blog
+
+func (a ByDate) Len() int           { return len(a) }
+func (a ByDate) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByDate) Less(i, j int) bool { return a[i].Date[0] < a[j].Date[0] }
+
 ////////////////////////////////////////////////////////////////////////////
 // Global variables definitions
 
 var progname = "csdnblog"
 var buildTime = "2016-10-20"
+
+var resultFolder = "/tmp/r/"
 
 ////////////////////////////////////////////////////////////////////////////
 // Function definitions
@@ -43,9 +53,42 @@ func main() {
 	fc, e := ioutil.ReadFile(os.Args[1])
 	check(e)
 
+	os.Mkdir(resultFolder, os.ModePerm)
+	fndx, err := os.Create(resultFolder + "00Index.html")
+	check(err)
+	defer fndx.Close()
+
+	fndx.WriteString("<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\r\n<style>\r\nbody {background-color: LightCyan;}\r\nh1   {color: blue;}\r\n</style>\r\n</head>\r\n<body>\r\n<h1>Index</h1>\r\n")
+
 	var blogs []Blog
 	json.Unmarshal(fc, &blogs)
-	fmt.Printf("Results: %v\n", blogs)
+	//fmt.Printf("Results: %v\n", blogs)
+
+	sort.Sort(ByDate(blogs))
+	for i, b := range blogs {
+		_ = i
+		if i > 5 {
+			break
+		}
+
+		fname := b.Title[0] + ".html"
+		fileo, err := os.Create(resultFolder + fname) // b.Date[0][:10] + "_" +
+		check(err)
+
+		fmt.Fprintf(fndx, "\r\n<a href='%s'><h2>%s %s</h2></a>\r\n",
+			fname, b.Date[0][:10], b.Title[0])
+		fileo.WriteString("<!DOCTYPE html>\r\n<html>\r\n<head>\r\n<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />\r\n<style>\r\nbody {background-color: LightCyan;}\r\nh1   {color: blue;}\r\n</style>\r\n</head>\r\n<body>\r\n")
+		fmt.Fprintf(fileo, "<h1>%s</h1><p>\r\nFrom %s<p>\r\n"+
+			"<address>&nbsp;&nbsp;%s</address><p>\r\n",
+			b.Title[0], b.Url, b.Info[0])
+		fileo.WriteString(b.Blog[0])
+		fileo.WriteString("\r\n</body>\r\n</html>\r\n")
+
+		fileo.Close()
+	}
+
+	fndx.WriteString("\r\n\r\n</body>\r\n</html>\r\n")
+
 }
 
 ////////////////////////////////////////////////////////////////////////////
